a239e2d7536b6dc948f722989602189e1e3657db
HSBallina <hakan@badpixel.se>
2024-05-22 14:42:49 +0200

    Refactor time sync

--- weather-station-edge/weather-station-edge.ino
+++ weather-station-edge/weather-station-edge.ino
@@ -3,29 +3,30 @@
 #include <WiFi.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
-#include "AzureIoT.h"
 
+#include "AzureIoT.h"
 #include "secrets.h"
 
 #define SERIAL_LOGGER_BAUD_RATE 115200
 
 #define NTP_SERVERS "pool.ntp.org", "time.nist.gov"
-
 #define CET_TZ "CET-1CEST,M3.5.0,M10.5.0/3"
+#define VERSION "20240522d"
 
 Adafruit_BME280 bmeOut;
 Adafruit_BME280 bmeIn;
-const char* VERSION = "20240522b";
 
 static void initTime(String timezone);
-static void logging_function(log_level_t log_level, char const* const format, ...);
+static void loggingFunction(log_level_t log_level, char const *const format, ...);
 static void connectWifi();
 
-struct readings {
+struct readings
+{
   float temperature, humidity, pressure;
 };
 
-struct sensor {
+struct sensor
+{
   Adafruit_BME280 sensor;
   String name;
   uint8_t address;
@@ -34,15 +35,16 @@ struct sensor {
 sensor outdoor = {bmeOut, "outdoor", 0x77};
 sensor indoor = {bmeIn, "indoor", 0x76};
 
-
-void setup() {
+void setup()
+{
   Serial.begin(SERIAL_LOGGER_BAUD_RATE);
 
-  while(!Serial) {
+  while (!Serial)
+  {
     delay(100);
   }
 
-  set_logging_function(logging_function);
+  set_logging_function(loggingFunction);
 
   Serial.println("");
   LogInfo("Weather Station %s", VERSION);
@@ -60,29 +62,35 @@ void setup() {
   Serial.println("");
 }
 
-void loop() {
+void loop()
+{
   delay(10000);
   dumpReadings(outdoor);
   delay(10000);
   dumpReadings(indoor);
 }
 
-void initSensor(sensor &s) {
+void initSensor(sensor &s)
+{
   LogInfo("Initializing sensor %s", s.name);
 
-  if(!s.sensor.begin(s.address)) {
+  if (!s.sensor.begin(s.address))
+  {
     LogError("Failed to initialize sensor %s", s.name);
-    while(1);
+    while (1)
+      ;
   }
 
   LogInfo("Initializing sensor %s done", s.name);
 }
 
-readings getReadings(sensor &s) {
-    return {s.sensor.readTemperature(), s.sensor.readHumidity(), s.sensor.readPressure() / 100.0F};
+readings getReadings(sensor &s)
+{
+  return {s.sensor.readTemperature(), s.sensor.readHumidity(), s.sensor.readPressure() / 100.0F};
 }
 
-void dumpReadings(sensor &s) {
+void dumpReadings(sensor &s)
+{
   readings r = getReadings(s);
 
   String payload = "{\"sensor\": \"" + s.name + "\", \"temperature\": " + String(r.temperature) + ", \"humidity\": " + String(r.humidity) + ", \"pressure\": " + String(r.pressure) + "}";
@@ -99,14 +107,17 @@ static void connectWifi()
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
 
   int i = 0;
-  while(WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(100);
     Serial.print(".");
     i++;
-    if(i > 10) {
+    if (i > 10)
+    {
       Serial.println("");
       LogError("Failed to connect to WiFi %s", WIFI_SSID);
-      while(1);
+      while (1)
+        ;
     }
   }
 
@@ -115,33 +126,45 @@ static void connectWifi()
   LogInfo("WiFi connected, IP address: %s", WiFi.localIP().toString().c_str());
 }
 
-void setTimezone(String timezone) {
-  LogInfo("Setting timezone to %s", timezone.c_str());
-  setenv("TZ", timezone.c_str(), 1);
-  tzset();
-}
-
-static void initTime(String timezone) {
-  struct tm timeinfo;
-
+static void initTime(String timezone)
+{
   LogInfo("Setting time using SNTP");
 
+  struct tm timeinfo;
   configTime(0, 0, NTP_SERVERS);
-  if(!getLocalTime(&timeinfo, 1000UL)) {
-    LogError("Failed to obtain time");
-    while(1);
+
+  Serial.print(".");
+
+  int i = 0;
+  while(!getLocalTime(&timeinfo, 2000UL)) {
+    Serial.print(".");
+    i++;
+    if (i > 10)
+    {
+      Serial.println("");
+      LogError("Failed to connect to WiFi %s", WIFI_SSID);
+      while (1)
+        ;
+    }
+    delay(5000);
   }
 
-  setTimezone(timezone);
+  Serial.println("");
+
+  setenv("TZ", timezone.c_str(), 1);
+  tzset();
+
+  LogInfo("Time set to %s", asctime(&timeinfo));
 }
 
-static void logging_function(log_level_t log_level, char const* const format, ...)
+static void loggingFunction(log_level_t log_level, char const *const format, ...)
 {
   struct tm ptm;
 
-  if(!getLocalTime(&ptm, 500UL)) {
+  if (!getLocalTime(&ptm, 500UL))
+  {
     time_t now = time(NULL);
-    struct tm* ptmp = gmtime(&now);
+    struct tm *ptmp = gmtime(&now);
     ptm = *ptmp;
   }
 
